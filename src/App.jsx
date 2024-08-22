1663e7c35aca06d50990393894b441fdd1edabea
Danil L <danillitvinenkod00@gmail.com>
2024-08-22 13:32:56 +0300

    Initializing the Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Body from "./layouts/Body/Body";
+import Menu from "./layouts/Menu/Menu";
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/Body/Body.jsx
@@ -0,0 +1,5 @@
+import styles from "./Body.module.css";
+
+export default function Body() {
+  return <div className={styles.body}>Body</div>;
+}
